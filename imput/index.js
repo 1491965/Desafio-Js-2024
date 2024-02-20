26323161f3acf3379182351d2f69f8eb511ee6f9
Valquiria Pereira dos Santos <valquiriapereiradossantos@gmail.com>
2024-02-20 16:56:21 -0300

    iniciando o desafio

---
+++ imput/index.js
@@ -0,0 +1,13 @@
+const $output = document.querySelector('.output')
+const $input = document.querySelector('.text-field')
+const $form = document.querySelector('.form')
+
+
+$form.addEventListener('submit', function(event){
+    event.preventDefault()
+    $output.textContent = $input.value
+})
+
+$input.addEventListener('input', function(){
+    $output.textContent = $input.value
+})
